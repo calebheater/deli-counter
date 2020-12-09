2fb3f408f085111ff6a0472a339a8d3f7d57c384
Caleb <calebheater@gmail.com>
2020-12-08 22:26:46 -0500

    commit tree 526e740f3427d231f0196ca96ebea4c5a0d7f91e

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+def line(katz_deli)
+    if katz_deli.empty? == true
+        puts "The line is currently empty."
+    else
+        customers = "The line is currently:"
+        katz_deli.each_with_index do |name, i|
+            customers += " #{i + 1}. #{name}"
+        end
+        puts customers
+    end
+end
+
+
+def take_a_number(katz_deli, customer)
+    katz_deli << customer
+    puts "Welcome, #{customer}. You are number #{katz_deli.length} in line."
+end
+
+
+def now_serving(katz_deli)
+    if katz_deli.empty?
+        puts "There is nobody waiting to be served!"
+    else
+        puts "Currently serving #{katz_deli.shift}."
+    end
+end
